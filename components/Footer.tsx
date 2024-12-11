e28ed86b4f886429dcc3f21302431dc99c0407e3
viniciusK-PC <viniciuskb36@gmail.com>
2024-12-11 20:51:21 -0300

    Removendo Logo

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,13 +13,7 @@ const Footer = () => {
         </h2>
         <div className="mx-auto max-w-screen-2xl px-6 lg:px-8 pt-24 pb-14">
           <div className="xl:grid xl:grid-cols-3 xl:gap-8">
-            <Image
-              src="/logo v1.png"
-              alt="Singitronic logo"
-              width={250}
-              height={250}
-              className="h-auto w-auto"
-            />
+           
             <div className="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
               <div className="md:grid md:grid-cols-2 md:gap-8">
                 <div>
